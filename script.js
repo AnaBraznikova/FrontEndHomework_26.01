bc4541965ab813f3d9ef843be83d724123ccbd8a
AnaBraznikova <116310193+AnaBraznikova@users.noreply.github.com>
2023-02-23 11:31:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// -------1 задача-----------
+
+// let arr1 = [1, 2, 3, 4, 5]
+//  for (let i = 0; i < arr1.length; i++) {
+//    document.write(arr1[i]);;
+//  }
+
+// ------2 задача--------------
+
+// for (let i = 1; i < 101; i++) {
+//   document.write(i + "<br>");
+// }
+
+// ------3 задача--------------
+
+// for (let i = 11; i < 34; i++) {
+//     document.write(i + "<br>");
+//     }
+
+// ------4 задача--------------
+
+// let arr2 = [1, 2, 3, 4, 5];
+// let result = 0;
+// for (let i = 0; i < arr2.length; i++) {
+//   result += arr2[i];
+// }
+// console.log(result);
